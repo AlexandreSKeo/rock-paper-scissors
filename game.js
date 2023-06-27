4280dff61f45687c27a33ab3bce5af27da8f28ad
CodeMarrow <alexandreskeo90@gmail.com>
2023-06-26 21:01:31 -0400

    Feat: function for randomized computer choice

---
+++ game.js
@@ -0,0 +1,12 @@
+// function that contains the logic for the computer to play rock paper scissors
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
